70f5c6b22aa79ca71a937ef8753fd6cc80f2a954
madcido <fabowow@gmail.com>
2020-08-01 21:58:41 -0300

    Updates

---
+++ precache-manifest.5b381ceaf521f74bdd59f30d3cd147c5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09bf171471389b1ba1ce16ec7dfaa15e",
+    "url": "/jnmoveis/index.html"
+  },
+  {
+    "revision": "b4deb81c7fbfd44a075c",
+    "url": "/jnmoveis/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1b006826288712979fe2",
+    "url": "/jnmoveis/static/js/2.668dae4a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jnmoveis/static/js/2.668dae4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4deb81c7fbfd44a075c",
+    "url": "/jnmoveis/static/js/main.21dc9a6c.chunk.js"
+  },
+  {
+    "revision": "79a2db2d51ddcb95798c",
+    "url": "/jnmoveis/static/js/runtime-main.551e88cc.js"
+  },
+  {
+    "revision": "2ee3eba60d64bdbcc58da820f7d20e65",
+    "url": "/jnmoveis/static/media/logoG.2ee3eba6.png"
+  },
+  {
+    "revision": "ceea3e3e9902d8d23b6963764c2f77f8",
+    "url": "/jnmoveis/static/media/slide1G.ceea3e3e.png"
+  },
+  {
+    "revision": "9a6ab95d0d56ab222e85666a7bf9fd88",
+    "url": "/jnmoveis/static/media/woodG.9a6ab95d.png"
+  }
+]);
